6d92d05b0e086e8819ddc02e1f53edda693c4a90
hunzsig <mzyhaohaoren@qq.com>
2019-12-25 00:25:38 +0800

    dialog现在支持table的buttons，同时会自动识别设置的key指，可能的话作为热键

--- lib/dialog.lua
+++ lib/dialog.lua
@@ -1,6 +1,19 @@
 -- [[对话框]]
 local hdialog = {}
 
+-- 自动根据key识别热键
+hdialog.hotkey = function(key)
+    if (key == nil) then
+        return 0
+    elseif (type(key) == "number") then
+        return key
+    elseif (type(key) == "string") then
+        return string.byte(key, 1)
+    else
+        return 0
+    end
+end
+
 -- 创建一个新的对话框
 hdialog.create = function(whichPlayer, options, call)
     local d = cj.DialogCreate()
@@ -8,12 +21,17 @@ hdialog.create = function(whichPlayer, options, call)
     cj.DialogSetMessage(d, options.title)
     if (#options.buttons == hSys.getTableLen(options.buttons)) then
         for i = 1, #options.buttons, 1 do
-            local b = cj.DialogAddButton(d, options.buttons[i], 0)
-            btnKv[b] = options.buttons[i]
+            if (type(options.buttons[i]) == "table") then
+                local b = cj.DialogAddButton(d, options.buttons[i].label, hdialog.hotkey(options.buttons[i].value))
+                btnKv[b] = options.buttons[i].value
+            else
+                local b = cj.DialogAddButton(d, options.buttons[i], hdialog.hotkey(options.buttons[i]))
+                btnKv[b] = options.buttons[i]
+            end
         end
     else
-        for k, v in pairs(options.buttons) do
-            local b = cj.DialogAddButton(d, v, 0)
+        for k, v in ipairs(options.buttons) do
+            local b = cj.DialogAddButton(d, v, hdialog.hotkey(v))
             btnKv[b] = k
         end
     end
