b9c82838e485a2d4b017361ec259bc36329280cf
hunzsig <mzyhaohaoren@qq.com>
2019-12-24 23:12:36 +0800

    对话框现在支持关联table及序列table

--- lib/dialog.lua
+++ lib/dialog.lua
@@ -6,26 +6,38 @@ hdialog.create = function(whichPlayer, options, call)
     local d = cj.DialogCreate()
     local btnKv = {}
     cj.DialogSetMessage(d, options.title)
-    for i = 1, #options.buttons, 1 do
-        local b = cj.DialogAddButton(d, options.buttons[i], 0)
-        btnKv[b] = options.buttons[i]
+    if (#options.buttons == hSys.getTableLen(options.buttons)) then
+        for i = 1, #options.buttons, 1 do
+            local b = cj.DialogAddButton(d, options.buttons[i], 0)
+            btnKv[b] = options.buttons[i]
+        end
+    else
+        for k, v in pairs(options.buttons) do
+            local b = cj.DialogAddButton(d, v, 0)
+            btnKv[b] = k
+        end
     end
     local dtg = cj.CreateTrigger()
-    cj.TriggerAddAction(dtg, function()
-        local tri_d = cj.GetClickedDialog()
-        local tri_b = cj.GetClickedButton()
-        local tri_bi = btnKv[tri_b]
-        call(tri_bi)
-        cj.DialogClear(tri_d)
-        cj.DialogDestroy(tri_b)
-        cj.DisableTrigger(cj.GetTriggeringTrigger())
-        cj.DestroyTrigger(cj.GetTriggeringTrigger())
-    end)
+    cj.TriggerAddAction(
+        dtg,
+        function()
+            local tri_d = cj.GetClickedDialog()
+            local tri_b = cj.GetClickedButton()
+            local tri_bi = btnKv[tri_b]
+            call(tri_bi)
+            cj.DialogClear(tri_d)
+            cj.DialogDestroy(tri_b)
+            cj.DisableTrigger(cj.GetTriggeringTrigger())
+            cj.DestroyTrigger(cj.GetTriggeringTrigger())
+        end
+    )
     cj.TriggerRegisterDialogEvent(dtg, d)
     if (whichPlayer == nil) then
         for i = 1, bj_MAX_PLAYERS, 1 do
-            if (cj.GetPlayerController(hplayer.players[i]) == MAP_CONTROL_USER
-                and cj.GetPlayerSlotState(hplayer.players[i]) == PLAYER_SLOT_STATE_PLAYING) then
+            if
+                (cj.GetPlayerController(hplayer.players[i]) == MAP_CONTROL_USER and
+                    cj.GetPlayerSlotState(hplayer.players[i]) == PLAYER_SLOT_STATE_PLAYING)
+             then
                 whichPlayer = hplayer.players[i]
                 break
             end
