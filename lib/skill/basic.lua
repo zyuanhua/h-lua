9610349446fff267de10585f3dc99f5e1e41a5e5
hunzsig <mzyhaohaoren@qq.com>
2020-02-02 22:35:21 +0800

    无敌的作用机制改为技能的Avul
    这样is方法的无敌判定才能正确的生效

--- lib/skill/basic.lua
+++ lib/skill/basic.lua
@@ -23,7 +23,7 @@ hskill.invulnerable = function(whichUnit, during, effect)
     if (during < 0) then
         during = 0.00 -- 如果设置持续时间错误，则0秒无敌，跟回避效果相同
     end
-    cj.SetUnitInvulnerable(whichUnit, true)
+    cj.UnitAddAbility(whichUnit, "Avul")
     if (during > 0 and effect ~= nil) then
         heffect.bindUnit(effect, whichUnit, "origin", during)
     end
@@ -32,7 +32,7 @@ hskill.invulnerable = function(whichUnit, during, effect)
         function(t, td)
             htime.delDialog(td)
             htime.delTimer(t)
-            cj.SetUnitInvulnerable(whichUnit, false)
+            cj.UnitRemoveAbility(whichUnit, "Avul")
         end
     )
 end
