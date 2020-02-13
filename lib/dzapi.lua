3bd04e2c9bf4cf4b184c3711c320da689308a0b2
hunzsig <mzyhaohaoren@qq.com>
2020-02-14 02:22:28 +0800

    封装dzapi

--- lib/dzapi.lua
+++ lib/dzapi.lua
@@ -28,81 +28,99 @@ end
 
 -- 服务器存档
 hdzapi.server = {}
+-- 读取服务器存档是否成功，没有开通或这服务器崩了返回false
+hdzapi.server.ready = function(whichPlayer)
+    if (whichPlayer == nil) then
+        return false
+    end
+    return japi.GetPlayerServerValueSuccess(whichPlayer)
+end
+
 -- save / load
 hdzapi.server.save = function(whichPlayer, key, data)
-    japi.DzAPI_Map_SaveServerValue(whichPlayer, key, data)
+    if (data == nil) then
+        return
+    end
+    if (hdzapi.server.ready(whichPlayer) == true) then
+        japi.DzAPI_Map_SaveServerValue(whichPlayer, key, data)
+    end
 end
+
 hdzapi.server.load = function(whichPlayer, key)
-    return japi.DzAPI_Map_GetServerValue(whichPlayer, key)
+    if (hdzapi.server.ready(whichPlayer) == true) then
+        return japi.DzAPI_Map_GetServerValue(whichPlayer, key)
+    end
 end
+
+-- 清理服务器存档数据
+hdzapi.server.clear = function(whichPlayer, key)
+    if (hdzapi.server.ready(whichPlayer) == true) then
+        japi.DzAPI_Map_SaveServerValue(whichPlayer, key, nil)
+    end
+end
+
 -- 封装的服务器存档 get / set
 hdzapi.server.set = {
     int = function(whichPlayer, key, data)
-        japi.DzAPI_Map_SaveServerValue(whichPlayer, "I" .. key, data or 0)
+        hdzapi.server.save(whichPlayer, "I" .. key, data or 0)
     end,
     real = function(whichPlayer, key, data)
-        japi.DzAPI_Map_SaveServerValue(whichPlayer, "R" .. key, data or 0)
+        hdzapi.server.save(whichPlayer, "R" .. key, data or 0)
     end,
     bool = function(whichPlayer, key, data)
         local b = "0"
         if (data == true) then
             b = "1"
         end
-        japi.DzAPI_Map_SaveServerValue(whichPlayer, "B" .. key, data or 0)
+        hdzapi.server.save(whichPlayer, "B" .. key, data or 0)
     end,
     str = function(whichPlayer, key, data)
-        japi.DzAPI_Map_SaveServerValue(whichPlayer, "S" .. key, data or nil)
+        hdzapi.server.save(whichPlayer, "S" .. key, data)
     end,
     unit = function(whichPlayer, key, data)
-        local id = string.id2char(cj.GetUnitTypeId(data))
-        japi.DzAPI_Map_SaveServerValue(whichPlayer, "S" .. key, id, data or nil)
+        hdzapi.server.save(whichPlayer, "S" .. key, hunit.getId(data))
     end,
     item = function(whichPlayer, key, data)
-        local id = string.id2char(cj.GetItemTypeId(data))
-        japi.DzAPI_Map_SaveServerValue(whichPlayer, "S" .. key, id, data or nil)
+        hdzapi.server.save(whichPlayer, "S" .. key, hitem.getId(data))
     end
 }
 hdzapi.server.get = {
     int = function(whichPlayer, key)
-        local val = japi.DzAPI_Map_GetServerValue(whichPlayer, "I" .. key) or 0
+        local val = hdzapi.server.load(whichPlayer, "I" .. key) or 0
         if (val == "") then
             val = 0
         end
         return math.floor(val)
     end,
     real = function(whichPlayer, key)
-        local val = japi.DzAPI_Map_GetServerValue(whichPlayer, "R" .. key) or 0
+        local val = hdzapi.server.load(whichPlayer, "R" .. key) or 0
         if (val == "") then
             val = 0
         end
         return math.round(val)
     end,
     bool = function(whichPlayer, key)
-        local b = japi.DzAPI_Map_GetServerValue(whichPlayer, "B" .. key)
+        local b = hdzapi.server.load(whichPlayer, "B" .. key)
         if (b == "1") then
             return true
         end
         return false
     end,
     str = function(whichPlayer, key)
-        return japi.DzAPI_Map_GetServerValue(whichPlayer, "S" .. key) or ""
+        return hdzapi.server.load(whichPlayer, "S" .. key) or ""
     end,
     unit = function(whichPlayer, key)
-        local id = japi.DzAPI_Map_GetServerValue(whichPlayer, "S" .. key) or ""
+        local id = hdzapi.server.load(whichPlayer, "S" .. key) or ""
         if (string.len(id) > 0) then
             return string.char2id(id)
         end
         return ""
     end,
     item = function(whichPlayer, key)
-        local id = japi.DzAPI_Map_GetServerValue(whichPlayer, "S" .. key) or ""
+        local id = hdzapi.server.load(whichPlayer, "S" .. key) or ""
         if (string.len(id) > 0) then
             return string.char2id(id)
         end
         return ""
     end
 }
--- 清理服务器数据
-hdzapi.server.clear = function(whichPlayer, key)
-    japi.DzAPI_Map_SaveServerValue(whichPlayer, key, nil)
-end
