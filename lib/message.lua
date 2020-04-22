116d16dcbfa8023a18e7d5a337e34934e2ed7b79
hunzsig <mzyhaohaoren@qq.com>
2020-04-23 01:11:06 +0800

    message删除了echoXX、echo00方法

--- lib/message.lua
+++ lib/message.lua
@@ -1,25 +1,28 @@
--- 消息
+---@class hmessage 消息
 hmessage = {}
 
--- 在屏幕打印信息给所有玩家
-hmessage.echo = function(msg, duration)
-    for i = 0, bj_MAX_PLAYERS - 1, 1 do
+--- 在屏幕打印信息给所有玩家
+---@param msg string
+---@param duration number
+---@param x number 可选，屏幕x处
+---@param y number 可选，屏幕y处
+---@param whichPlayer userdata 可选，打印给某玩家
+hmessage.echo = function(msg, duration, x, y, whichPlayer)
+    x = x or 0
+    y = y or 0
+    if (whichPlayer == nil) then
+        for i = 0, bj_MAX_PLAYERS - 1, 1 do
+            if (duration == nil or duration < 5) then
+                cj.DisplayTextToPlayer(cj.Player(i), x, y, msg)
+            else
+                cj.DisplayTimedTextToPlayer(cj.Player(i), x, y, duration, msg)
+            end
+        end
+    else
         if (duration == nil or duration < 5) then
-            cj.DisplayTextToPlayer(cj.Player(i), 0, 0, msg)
+            cj.DisplayTextToPlayer(whichPlayer, x, y, msg)
         else
-            cj.DisplayTimedTextToPlayer(cj.Player(i), 0, 0, duration, msg)
+            cj.DisplayTimedTextToPlayer(whichPlayer, x, y, duration, msg)
         end
     end
-end
--- 在屏幕(x,y)处打印信息给某玩家
-hmessage.echoXY = function(whichPlayer, msg, x, y, duration)
-    if (duration == nil or duration < 5) then
-        cj.DisplayTextToPlayer(whichPlayer, x, y, msg)
-    else
-        cj.DisplayTimedTextToPlayer(whichPlayer, x, y, duration, msg)
-    end
-end
--- 在屏幕(0.0)处打印信息给某玩家
-hmessage.echo00 = function(whichPlayer, msg, duration)
-    hmessage.echoXY(whichPlayer, msg, 0, 0, duration)
 end
